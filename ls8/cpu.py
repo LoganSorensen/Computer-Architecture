043712af78a2ff5289e7a20df85b15d9cce91ebd
Logan Sorensen <loganmsorensen@gmail.com>
2020-06-16 15:20:10 -0700

    day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,12 @@ class CPU:
         self.pc = 0
         self.reg = [0] * 8
         self.ram = [0] * 256
+        self.branch_table = {
+            0b00000001: self.HLT,
+            0b10000010: self.LDI,
+            0b01000111: self.PRN,
+            0b10100010: self.MUL,
+        }
 
     def ram_read(self, mar):
         # mar is the address being read
@@ -20,26 +26,20 @@ class CPU:
         # mdr is the data being written
         self.ram[mar] = mdr
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(filename) as f:
+            for line in f:
+                line = line.split('#')
+                try:
+                    v = int(line[0], 2)
+                except ValueError:
+                    continue
+                self.ram[address] = v
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -70,28 +70,32 @@ class CPU:
 
         print()
 
+    def HLT(self, operand_a, operand_b):
+        sys.exit()
+
+    def LDI(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+
+    def PRN(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        self.pc += 2
+
+    def MUL(self, operand_a, operand_b):
+        self.reg[operand_a] *= self.reg[operand_b]
+        self.pc += 3
+
     def run(self):
         """Run the CPU."""
-        HLT = 0b00000001
-        LDI = 0b10000010
-        PRN = 0b01000111
-        running = True
 
-        while running:
+        while True:
             # ir == Instruction Register
             ir = self.ram_read(self.pc)
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
 
-            if ir == HLT:
-                running = False
-            elif ir == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif ir == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
+            if ir in self.branch_table:
+                self.branch_table[ir](operand_a, operand_b)
             else:
                 print(f'Unknown instruction {ir} at address {self.pc}')
-                running = False
                 sys.exit()
